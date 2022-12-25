31f0f7610accb04a7280a838a17c55decb3c7cca
Vladislav <vnl.i@ya.ru>
2022-12-25 21:57:18 +0300

    Домашняя задача 3 семинар 9

---
+++ DZS9.003/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 3: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Prompt(string message)
+{
+    Console.Write($"{message} > ");
+    string input = Console.ReadLine();
+    return Convert.ToInt32(input);
+}
+
+int A(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (n == 0)
+    {
+        return A(m - 1, 1);
+    }
+    return A(m - 1, A(m, n - 1));
+}
+
+int n = Prompt("Введите N");
+int m = Prompt("Введите M");
+
+Console.WriteLine(A(m, n));
